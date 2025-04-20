c30a50ddd1bfb1082ea34929b0fa386066f21130
VitsVi <jigulsky.vitalik@yandex.ru>
2025-04-20 14:00:59 +0300

    check-2

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'Название'
     app_description: str = 'Описание'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
 
     class Config:
